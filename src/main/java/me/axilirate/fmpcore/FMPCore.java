e68313be37c6d91f60157ff3a1a41b766983cae0
axilirate <56825186+axilirate@users.noreply.github.com>
2022-05-24 01:15:27 +0300

    update

--- src/main/java/me/axilirate/fmpcore/FMPCore.java
+++ src/main/java/me/axilirate/fmpcore/FMPCore.java
@@ -1,24 +1,26 @@
 package me.axilirate.fmpcore;
 
 import github.scarsz.discordsrv.DiscordSRV;
+import github.scarsz.discordsrv.dependencies.jda.api.entities.Guild;
+import github.scarsz.discordsrv.dependencies.jda.api.entities.Member;
 import github.scarsz.discordsrv.dependencies.jda.api.entities.User;
 import github.scarsz.discordsrv.util.DiscordUtil;
+import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.player.PlayerBucketFillEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.entity.EntityExhaustionEvent;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.event.player.*;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -26,6 +28,8 @@ import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitScheduler;
 import org.bukkit.scoreboard.*;
 
+import java.awt.*;
+import java.text.DecimalFormat;
 import java.util.UUID;
 
 
@@ -34,6 +38,9 @@ public final class FMPCore extends JavaPlugin implements Listener {
 
     NamespacedKey buildCooldownKey = new NamespacedKey(this, "buildcooldown");
     NamespacedKey breakCooldownKey = new NamespacedKey(this, "breakcooldown");
+
+
+    float hungerMultiplayer = 0.00036f;
 
 
 
@@ -57,11 +64,15 @@ public final class FMPCore extends JavaPlugin implements Listener {
 
                     Objective objective = player.getScoreboard().getObjective("buildbreakcooldowns");
 
+
+
+
                     Score buildScore = objective.getScore("build cooldown: ");
                     buildScore.setScore(playerBuildCooldown);
 
                     Score breakScore = objective.getScore("break cooldown: ");
                     breakScore.setScore(playerBreakCooldown);
+
 
 
 
@@ -109,7 +120,35 @@ public final class FMPCore extends JavaPlugin implements Listener {
     @EventHandler
     public void onPlayerJoinEvent(PlayerJoinEvent event) {
         Player player = event.getPlayer();
+
+
+        Color color = null;
+        String discordId = DiscordSRV.getPlugin().getAccountLinkManager().getDiscordId(player.getUniqueId());
+        if (discordId != null){
+            Guild guild = DiscordSRV.getPlugin().getMainGuild();
+            Member member = guild.getMemberById(discordId);
+            color = member.getColor();
+        }
+
+
+
+        if (color == null){
+            color = Color.WHITE;
+        }
+
+
+
+
+        player.setDisplayName(ChatColor.of(color) + player.getName());
+        player.setPlayerListName(ChatColor.of(color) + player.getName());
+
+
         PersistentDataContainer playerData = player.getPersistentDataContainer();
+
+
+
+        player.setBedSpawnLocation(null);
+
 
         Boolean newKey = false;
 
@@ -303,12 +342,16 @@ public final class FMPCore extends JavaPlugin implements Listener {
 
 
 
-
     @EventHandler
     public void onEntityDamage(EntityDamageEvent event) {
 
         if (event.getEntity() instanceof Player){
             Location location = event.getEntity().getLocation();
+
+            if (event.getEntity().getWorld().getName().equals("world_the_end")){
+                return;
+            }
+
             if (Math.abs(location.getBlockX()) < 1000 && Math.abs(location.getBlockZ()) < 1000){
                 event.setCancelled(true);
             }
@@ -349,19 +392,33 @@ public final class FMPCore extends JavaPlugin implements Listener {
 
     public void updateDisplayTitle(Player player){
 
+
+
+
+
         Objective objective = player.getScoreboard().getObjective("buildbreakcooldowns");
 
         Location location = player.getLocation();
 
-        int blocksLeft = Math.max(Math.abs(location.getBlockX()), Math.abs(location.getBlockZ()));
+
+        int x = Math.abs(location.getBlockX());
+        int z = Math.abs(location.getBlockZ());
+
+        float rate = Math.max((float) x, (float) z) * hungerMultiplayer;
+
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
+
+        String formattedRate = decimalFormat.format(rate);
+
+        int blocksLeft = Math.max(Math.abs(x), Math.abs(z));
 
         if (Math.abs(location.getBlockX()) > 2500 || Math.abs(location.getBlockZ()) > 2500){
 
             player.spigot().sendMessage(
                     ChatMessageType.ACTION_BAR,
-                    new TextComponent("Protection: Off | Slow Mode: Off"));
+                    new TextComponent("Protection: Off | Slow Mode: Off | Hunger Rate: " + formattedRate));
 
-            objective.setDisplayName(ChatColor.BOLD + "Last Zone In " + Integer.toString(blocksLeft - 2500) + " Blocks");
+            objective.setDisplayName(ChatColor.BOLD + "3rd Layer In " + Integer.toString(blocksLeft - 2500) + " Blocks");
 
             return;
         }
@@ -374,20 +431,119 @@ public final class FMPCore extends JavaPlugin implements Listener {
 
             player.spigot().sendMessage(
                     ChatMessageType.ACTION_BAR,
-                    new TextComponent("Protection: Off | Slow Mode: On"));
+                    new TextComponent("Protection: Off | Slow Mode: On | Hunger Rate: " + formattedRate));
 
 
 
-            objective.setDisplayName(ChatColor.BOLD + "Next Zone In " + Integer.toString(2500 - blocksLeft) + " Blocks");
+            objective.setDisplayName(ChatColor.BOLD + "3rd Layer In " + Integer.toString(2500 - blocksLeft) + " Blocks");
+            return;
+        }
+
+
+
+
+        objective.setDisplayName(ChatColor.BOLD + "2nd Layer In " + Integer.toString(1000 - blocksLeft) + " Blocks");
+
+
+        if (player.getWorld().getName().equals("world_the_end")){
+            player.spigot().sendMessage(
+                    ChatMessageType.ACTION_BAR,
+                    new TextComponent("Protection: Off | Slow Mode: On | Hunger Rate: " + formattedRate));
+
+
             return;
         }
 
 
         player.spigot().sendMessage(
                 ChatMessageType.ACTION_BAR,
-                new TextComponent("Protection: ON | Slow Mode: ON"));
+                new TextComponent("Protection: On | Slow Mode: On | Hunger Rate: " + formattedRate));
 
-        objective.setDisplayName(ChatColor.BOLD + "Next Zone In " + Integer.toString(1000 - blocksLeft) + " Blocks");
+
+
+
+    }
+
+
+
+    @EventHandler
+    public void onAsyncPlayerChat(AsyncPlayerChatEvent event){
+        Player player = event.getPlayer();
+
+        UUID uuid = player.getUniqueId();
+        String discordId = DiscordSRV.getPlugin().getAccountLinkManager().getDiscordId(uuid);
+        Guild guild = DiscordSRV.getPlugin().getMainGuild();
+        Member member = guild.getMemberById(discordId);
+        Color color = member.getColor();
+
+
+        if (color == null){
+            color = Color.WHITE;
+        }
+
+
+        event.setFormat(ChatColor.of(color) + player.getDisplayName() + ChatColor.WHITE + " » " + event.getMessage());
+    }
+
+
+
+
+
+    @EventHandler
+    public void onPlayerRespawn(PlayerRespawnEvent event){
+        event.setRespawnLocation(null);
+    }
+
+
+    @EventHandler
+    public void onPlayerBedEnter(PlayerBedEnterEvent event){
+        event.setCancelled(true);
+    }
+
+
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event){
+
+        if (event.getClickedBlock() == null){
+            return;
+        }
+
+        if (event.getAction() == Action.LEFT_CLICK_BLOCK){
+            return;
+        }
+
+
+        if (event.getClickedBlock().getBlockData() instanceof org.bukkit.block.data.type.Bed) {
+            event.setCancelled(true);
+        }
+    }
+
+
+
+    @EventHandler
+    public void onEntityExhaustion(EntityExhaustionEvent event){
+
+        if (event.getEntity() instanceof Player){
+            Player player = (((Player) event.getEntity()).getPlayer());
+
+            Location location = player.getLocation();
+            int x = Math.abs(location.getBlockX());
+            int z = Math.abs(location.getBlockZ());
+
+
+            float rate = Math.max((float) x, (float) z) * hungerMultiplayer;
+
+
+            event.setExhaustion(event.getExhaustion() * rate);
+
+
+
+
+
+        }
+
+
 
 
     }
